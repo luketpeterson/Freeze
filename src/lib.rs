bd67d1a809cbc9009752a01b7be3608db8ec37c3
Adam-Vandervorst <adam.vandervorst@gmail.com>
2025-03-02 15:19:58 +0100

    Add data size

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ use std::slice::SliceIndex;
 use std::ops::{Deref, DerefMut};
 use libc;
 
+#[repr(transparent)]
 pub struct LiquidVecRef<'alloc> { alloc: &'alloc mut BumpAlloc }
 
 impl <'alloc> LiquidVecRef<'alloc> {
+    #[inline(always)]
     pub fn finalize(self) -> &'alloc mut [u8] {
         unsafe {
             let ret = std::ptr::slice_from_raw_parts_mut(self.alloc.top_base, self.alloc.top_size);
@@ -17,6 +19,7 @@ impl <'alloc> LiquidVecRef<'alloc> {
         }
     }
 
+    #[inline(always)]
     fn extend_one(&mut self, item: u8) {
         unsafe {
             *self.alloc.top_base.add(self.alloc.top_size) = item;
@@ -24,12 +27,14 @@ impl <'alloc> LiquidVecRef<'alloc> {
         }
     }
 
+    #[inline(always)]
     fn extend_reserve(&mut self, additional: usize) {
         unsafe {
             libc::madvise(self.alloc.top_base.add(self.alloc.top_size) as _, additional, libc::MADV_WILLNEED);
         }
     }
 
+    #[inline(always)]
     pub fn extend_from_slice(&mut self, items: &[u8]) {
         unsafe {
             std::ptr::copy(items.as_ptr(), self.alloc.top_base.add(self.alloc.top_size), items.len());
@@ -37,13 +42,14 @@ impl <'alloc> LiquidVecRef<'alloc> {
         }
     }
 
+    #[inline(always)]
     pub fn extend_from_within<R>(&mut self, src: R) where R : std::slice::SliceIndex<[u8], Output = [u8]> {
         unsafe {
             self.extend_from_slice(&std::slice::from_raw_parts(self.alloc.top_base, self.alloc.top_size).as_ref()[src])
         }
     }
 
-    #[inline]
+    #[inline(always)]
     pub fn pop(&mut self) -> Option<u8> {
         if self.alloc.top_size == 0 {
             None
@@ -55,6 +61,7 @@ impl <'alloc> LiquidVecRef<'alloc> {
         }
     }
 
+    #[inline(always)]
     pub fn truncate(&mut self, len: usize) {
         if len > self.alloc.top_size {
             return;
@@ -64,6 +71,7 @@ impl <'alloc> LiquidVecRef<'alloc> {
 }
 
 impl <'alloc> std::borrow::Borrow<[u8]> for LiquidVecRef<'alloc> {
+    #[inline(always)]
     fn borrow(&self) -> &[u8] {
         unsafe {
             std::slice::from_raw_parts(self.alloc.top_base, self.alloc.top_size)
@@ -72,6 +80,7 @@ impl <'alloc> std::borrow::Borrow<[u8]> for LiquidVecRef<'alloc> {
 }
 
 impl <'alloc> std::borrow::BorrowMut<[u8]> for LiquidVecRef<'alloc> {
+    #[inline(always)]
     fn borrow_mut(&mut self) -> &mut [u8] {
         unsafe {
             std::slice::from_raw_parts_mut(self.alloc.top_base, self.alloc.top_size)
@@ -80,6 +89,7 @@ impl <'alloc> std::borrow::BorrowMut<[u8]> for LiquidVecRef<'alloc> {
 }
 
 impl <'alloc> Extend<u8> for LiquidVecRef<'alloc>  {
+    #[inline(always)]
     fn extend<T: IntoIterator<Item=u8>>(&mut self, iter: T) {
         iter.into_iter().for_each(|b| self.extend_one(b))
     }
@@ -87,16 +97,19 @@ impl <'alloc> Extend<u8> for LiquidVecRef<'alloc>  {
 
 impl <'alloc, I: SliceIndex<[u8]>> std::ops::Index<I> for LiquidVecRef<'alloc>  {
     type Output = I::Output;
+    #[inline(always)]
     fn index(&self, index: I) -> &Self::Output { std::ops::Index::index(self.deref(), index) }
 }
 
 impl <'alloc, I: SliceIndex<[u8]>> std::ops::IndexMut<I> for LiquidVecRef<'alloc>  {
+    #[inline(always)]
     fn index_mut(&mut self, index: I) -> &mut Self::Output { std::ops::IndexMut::index_mut(self.deref_mut(), index) }
 }
 
 impl <'alloc> std::ops::Deref for LiquidVecRef<'alloc> {
     type Target = [u8];
 
+    #[inline(always)]
     fn deref(&self) -> &Self::Target {
         unsafe {
             std::slice::from_raw_parts(self.alloc.top_base, self.alloc.top_size)
@@ -105,6 +118,7 @@ impl <'alloc> std::ops::Deref for LiquidVecRef<'alloc> {
 }
 
 impl <'alloc> std::ops::DerefMut for LiquidVecRef<'alloc> {
+    #[inline(always)]
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe {
             std::slice::from_raw_parts_mut(self.alloc.top_base, self.alloc.top_size)
@@ -125,10 +139,11 @@ pub struct BumpAllocRef {
 }
 
 impl BumpAllocRef {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self::new_with_address_space(32)
     }
-    fn new_with_address_space(bits: u8) -> Self {
+
+    pub fn new_with_address_space(bits: u8) -> Self {
         use libc::*;
         unsafe {
             let res = mmap(std::ptr::null_mut(), 1 << bits, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0);
@@ -142,7 +157,7 @@ impl BumpAllocRef {
             }
             *(res as *mut BumpAlloc) = BumpAlloc {
                 address_space: 1 << bits,
-                top_base: (res as *mut u8).add(size_of::<BumpAlloc>()),
+                top_base: (res as *mut u8).byte_add(size_of::<BumpAlloc>()),
                 top_size: 0,
             };
 
@@ -150,11 +165,33 @@ impl BumpAllocRef {
         }
     }
 
-    fn top(&self) -> LiquidVecRef {
+    pub fn top(&self) -> LiquidVecRef {
         unsafe {
             LiquidVecRef {
                 alloc: self.ptr.as_mut().unwrap_unchecked()
             }
+        }
+    }
+
+    unsafe fn data_range(&self) -> &[u8] {
+        let data_base = self.ptr.byte_add(size_of::<BumpAlloc>()) as *mut u8;
+        std::slice::from_raw_parts(data_base, ((*self.ptr).top_base as usize - data_base as usize) + (*self.ptr).top_size)
+    }
+
+    unsafe fn data_range_mut(&mut self) -> &mut [u8] {
+        let data_base = self.ptr.byte_add(size_of::<BumpAlloc>()) as *mut u8;
+        std::slice::from_raw_parts_mut(data_base, ((*self.ptr).top_base as usize - data_base as usize) + (*self.ptr).top_size)
+    }
+
+    pub fn data_size(&self) -> usize {
+        unsafe {
+            self.data_range().len()
+        }
+    }
+
+    pub fn dangerous(&self) -> bool {
+        unsafe {
+            (self.data_size() + size_of::<BumpAlloc>()) > (*self.ptr).address_space/2
         }
     }
 }
@@ -173,9 +210,9 @@ mod tests {
 
     #[test]
     fn basis() {
-        let alloc = BumpAllocRef::new();
+        let mut alloc = BumpAllocRef::new();
 
-        let s1 = {
+        let s1: &[u8] = {
             let mut v1 = alloc.top();
             v1.extend_from_slice(&[1, 2, 3]);
             v1.extend_one(4);
@@ -187,7 +224,7 @@ mod tests {
 
         assert_eq!(s1, [3, 2, 1, 4, 3, 2]);
 
-        let s2 = {
+        let s2: &[u8] = {
             let mut v1 = alloc.top();
             v1.extend_from_slice(&[10, 20, 30]);
             v1.extend_one(40);
@@ -198,5 +235,6 @@ mod tests {
         };
 
         assert_eq!(s2, [30, 20, 10, 40, 30, 20]);
+        assert_eq!(alloc.data_size(), (s1.len() + s2.len()));
     }
 }
